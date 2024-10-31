727b7cf7788a5e9034568fe155699f23e7a0b3eb
MerajAhmmed <merajahmmed6@gmail.com>
2024-10-31 23:20:38 +0600

    complte to separate componenet

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
 import Header from "./components/header/Header";
+import WeatherBord from "./components/weather/WeatherBord";
 
 export default function App() {
-  return <Header />;
+  return (
+    <div className="grid justify-center items-center h-screen mt-20">
+      <Header />
+      <main>
+        <section>
+          <WeatherBord />
+        </section>
+      </main>
+    </div>
+  );
 }
